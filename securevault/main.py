11c407f23cef5419dba874218038936b043c07c9
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2025-02-08 17:53:22 +0530

    File structure defined

--- securevault/main.py
+++ securevault/main.py
@@ -1,22 +1,48 @@
 import os
 from pathlib import Path
 from typing import Optional, Self
+import pickle
+from encoding.utils import TextEncoder
 
 
 VAULTS_PATH: Path = Path(os.environ['USERPROFILE']) / '.vaults' if os.name == 'nt' else Path('~').expanduser() / '.vaults'
 
 if not os.path.exists(VAULTS_PATH):
-    os.makedirs(VAULTS_PATH)
+    VAULTS_PATH.mkdir()
 
 
 class Vault:
     def __init__(self: Self) -> None:
-        self.vaults: list[str] = [vault_name.stem for vault_name in VAULTS_PATH.iterdir() if vault_name.is_dir()]
+        self.vaults: list[str] = [vault_name.stem.lower() for vault_name in VAULTS_PATH.iterdir() if vault_name.is_dir()]
         self.active_vault: str = self.vaults[0] if self.vaults else None
         self.vault_path: Path = VAULTS_PATH / self.active_vault if self.active_vault else None
 
-    def create_vault(self: Self, vault_name: str, encoder_file: Path | str) -> None:
-        ...
+    def create_vault(self: Self, vault_name: str, encoder: Path | str | TextEncoder) -> None:
+        if vault_name.lower() in self.vaults:
+            raise ValueError(f"Vault '{vault_name}' already exists")
+
+        vault_path = VAULTS_PATH / vault_name.lower()
+        vault_path.mkdir()
+
+        if isinstance(encoder, str | Path):
+            encoder_path = Path(encoder) if isinstance(encoder, str) else encoder
+            if encoder_path.is_file() and encoder_path.suffix == '.enc':
+                with open(encoder_path, 'rb') as enc_file:
+                    encoder = pickle.load(enc_file)
+                with open(vault_path / "encoder.enc", 'wb') as enc_file:
+                    pickle.dump(encoder, enc_file, protocol=4)
+            else:
+                raise FileNotFoundError(f"Encoder file not found at {encoder_path}")
+        elif isinstance(encoder, TextEncoder):
+            with open(vault_path / "encoder.enc", 'wb') as enc_file:
+                pickle.dump(encoder, enc_file, protocol=4)
+        else:
+            raise ValueError("encoder must be a TextEncoder instance or path to a '.enc' file.")
+
+        (vault_path / "index.idx").touch()
+        (vault_path / "data.vault").touch()
+
+        self.vaults.append(vault_name.lower())
 
     def open_vault(self: Self, vault_name: str) -> None:
         ...
@@ -45,3 +71,15 @@ class Vault:
     def del_vault(self: Self) -> None:
         ...
 
+
+if __name__ == '__main__':
+    from encoding.ciphers.substitution import CaesarCipher, AffineCipher
+    from encoding.utils import Pipeline
+    encoder = Pipeline([
+        (CaesarCipher(3), "caesar"),
+        (AffineCipher(2, 5), "affine")
+    ])
+    # vault = Vault()
+    # vault.create_vault("test_vault", encoder)
+
+    print(pickle.dumps(encoder))
